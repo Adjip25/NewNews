4cdc45bdb56eb905d71011f4014d91cfd5ceaaa4
Adjip25 <adjipratama18@gmail.com>
2023-11-15 13:17:17 +0700

    feat: created category

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,9 +1,11 @@
+const { Author } = require("../models");
+
 class AuthorController {
   static async register(req, res, next) {
     try {
       const { username, email, password, role } = req.body;
       const newUser = { username, email, password, role };
-      const response = await User.create(newUser);
+      const response = await Author.create(newUser);
       if (!response) {
         throw new Error("CREATE NEW USER FAILED");
       }
@@ -18,7 +20,30 @@ class AuthorController {
 
   static async login(req, res, next) {
     try {
-    } catch (error) {}
+      const { email, password } = req.body;
+      if (!email || !password) {
+        throw new Error("Please input email/password");
+      }
+
+      const user = await Author.findOne({
+        where: { email },
+      });
+
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      const isValidPassword = compareHash(password, user.password);
+      if (!isValidPassword) {
+        throw new Error("Invalid email/password");
+      }
+
+      const accessToken = signToken({ id: user.id, role: user.role });
+      res.status(200).json({ token: accessToken });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   }
 }
 
