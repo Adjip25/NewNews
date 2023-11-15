4b11c8c2d3f3b1a84e51921818fc09c65f746699
Adjip25 <adjipratama18@gmail.com>
2023-11-15 14:03:23 +0700

    feat: successfully created category

--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const author = require("./author");
 const article = require("./article");
+const category = require("./category");
 
 router.use(author);
 router.use(article);
+router.use(category);
 
 module.exports = router;
