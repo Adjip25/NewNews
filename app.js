954e6d3987c874be950e21de2405d49363b77806
Adjip25 <adjipratama18@gmail.com>
2023-11-15 14:02:01 +0700

    feat: add router for category

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ if (process.env.NODE_ENV !== "Production") {
 }
 const express = require("express");
 const router = require("./routers");
-const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
-app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Pinjam dulu ${port}`);
--- routers/author.js
+++ routers/author.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const authentication = require("../middlewares/authentication");
 const AuthorController = require("../controllers/AuthorController");
 const router = express.Router();
 
 router.post("/login", AuthorController.login);
 
-router.use(authentication);
 router.post("/register", AuthorController.register);
 
 module.exports = router;
